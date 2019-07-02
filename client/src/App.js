7455c49b60931b649ee9ac1947717f4028aeb589
kiplaa-bett <[betki.plaa@gmail.com]>
2019-07-02 14:21:28 +0300

    edit NavRar

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import NavBar from "./NavBar";
+//import HealthClinic from "./hlthclnc";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="hlthclnc-app">
+      <header className="hlthclnc-header">
+
+        <NavBar />
+
       </header>
+      <main className="hlthclnc-main">
+
+      </main>
     </div>
   );
 }
