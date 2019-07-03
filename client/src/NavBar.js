a8dfe87245a00a8255ab53b72687d456c5d15e43
kiplaa-bett <[betki.plaa@gmail.com]>
2019-07-04 02:12:32 +0300

    Finalizing NavBar

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -6,7 +6,7 @@ function NavBar() {
             <div className="navbar">
                 <nav className="hlthclnc-title">
                     <h1>Baraka Health Clinic</h1>
-                    <h2>"We Treat Jesus Heals"</h2>
+                    <h2>We Treat Jesus Heals</h2>
                 </nav>
                 <nav className="hlthclnc-nav">
                     <ul>
