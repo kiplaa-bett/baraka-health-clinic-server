1efeed52567371148eaec332f23b4f4455f43996
kiplaa-bett <[betki.plaa@gmail.com]>
2019-08-04 19:15:18 +0300

    linking Admin page with Docter page

--- client/src/AdminPage.js
+++ client/src/AdminPage.js
@@ -1,9 +1,18 @@
 import React from "react";
+import DocterForm from "./DocterForm";
+import DocterTable from "./DocterTable";
+import PatientForm from "./PatientForm";
+import PatientTable from "./PatientTable";
 
 function AdminPage() {
     return (
         <div className="home-container">
-            <h1>Admin Page</h1>
+            <h1>Docter</h1>
+            <DocterForm />
+            <DocterTable />
+            <h1>Patient</h1>
+            <PatientForm />
+            <PatientTable />
         </div>
     );
 }
--- client/src/Docter.js
+++ client/src/Docter.js
@@ -3,11 +3,25 @@ import React from "react";
 function Docter({ docter }) {
     const { Docter_id, Docter_Name, Department, Hospital_id } = docter;
     return (
-        <div>
-            <p>{Docter_id}</p>
-            <p>{Docter_Name}</p>
-            <p>{Department}</p>
-            <p>{Hospital_id}</p>
+        <div className="">
+            <table>
+                <thead>
+                    <tr>
+                        <th> NO </th>
+                        <th> DOCTER NAME </th>
+                        <th> DEPARTMENT </th>
+                        <th> HOSPITAL </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{Docter_id}</td>
+                        <td>{Docter_Name}</td>
+                        <td>{Department}</td>
+                        <td>{Hospital_id}</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 
